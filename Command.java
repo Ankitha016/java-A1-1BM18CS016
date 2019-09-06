7003b6f06d959371e3e13584cac750baa683e5fa
Ankitha016 <53806708+Ankitha016@users.noreply.github.com>
2019-09-06 15:18:37 +0530

    Add files via upload

---
+++ Command.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Command
+{
+	public static void main(String args[])
+	{
+		int i,a;
+		int large=-99;
+		for(i=0;i<args.length;i++)
+		{
+		a=Integer.parseInt(args[i]);
+		System.out.println(args[i]);
+		if(a>large)
+		{
+			large=a;
+		}
+		}
+		System.out.println("largest number is="+large);
+	}
+}
+			
+		
+	
